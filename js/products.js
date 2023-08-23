fa252bbe9271610226ff271399b5a2ce25512671
dayaburmi <dayanaburmida@gmail.com>
2023-08-23 17:50:17 -0300

    Cambios desde Develop

--- js/products.js
+++ js/products.js
@@ -1,1 +1,26 @@
-let buton = document.getElementById()
+const API_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const containerProducts = document.getElementById("containerProducts");
+
+fetch (API_URL)
+    .then(response =>{
+        return response.json();
+    })
+    .then(data => {
+        for (const product of data.products) {
+            containerProducts.innerHTML += `
+            <div class="row list-group-item d-flex justify-content-start">
+                <div class="col-3"> <img src="${product.image}" class="img-thumbnail img-fluid"/> </div>
+                    <div class="col-8">
+                        <h3>${product.name} - ${product.currency} ${product.cost}</h3>
+                        <p>${product.description}</p>
+                    </div>
+                <div class="col-1 text-muted p-0"> 
+                    <small>${product.soldCount} vendidos</small>
+                </div>
+            </div>
+            `
+        }
+    })
+    .catch(error =>{
+        console.log("Error", error);
+    })
