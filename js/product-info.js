1f7c2dc9707f55c90a6722485a423e4ce8492d46
BardesioDiego <diegobardesio@gmail.com>
2023-09-13 19:18:49 -0300

    Función mostrar productos

--- js/product-info.js
+++ js/product-info.js
@@ -1,40 +1,56 @@
-
 const productID = localStorage.getItem("productID");
-const API_URL = `https://japceibal.github.io/emercado-api/cats_products/${cat}.json`; //No se si está bien usar ${cat}
-
+const API_URL = `https://japceibal.github.io/emercado-api/products/${productID}.json`;
+const containerProduct = document.getElementById("containerProductInfo");
+document.addEventListener("DOMContentLoaded", () => {
+    obtainAndShowProductsInfo(API_URL)
+})
 // Hacer la solicitud fetch a la API
-fetch(API_URL)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`La solicitud fetch falló con estado ${response.status}`);
+async function obtainAndShowProductsInfo(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        showProduct(data);
+    } catch (error) {
+        console.log("error", error);
     }
-    return response.json(); // Parsear la respuesta JSON
-  })
-  .then(data => {
-    // Buscar el objeto con "id" igual a productID
-    const product = data.products.find(item => item.id === productID);
-    showProductInfo()
-    if (product) {
-      // El objeto con "id": productID fue encontrado
-      console.log(product);
-    } else {
-      // No se encontró ningún objeto con ese "id"
-      console.log("Producto no encontrado");
-    }
-  })
-  .catch(error => {
-    console.error("Error al obtener datos de la API:", error);
-  });
-
-
-function showProductInfo(id){
-
-    containerProductInfo.innerHTML += `
-    <h2>products.name</h2>
-    <h3>precio</h3>
-    <p>${products.cost}</p>
-    <br>
-    `;
 }
 
+function showProduct(product){
+containerProduct.innerHTML = ` 
+<div class="row mb-2">
+  <h1 class="border-bottom border-2 pt-5 pb-5 h2">${product.name}</h1>
+</div>
+<div class="row">
+  <div class="mb-4">
+    <h6 class="mb-0" class="mb-0"><b>Precio</b></h6>
+    <small>${product.currency} ${product.cost}</small>
+  </div>
+  <div class="mb-4">
+    <h6 class="mb-0"><b>Descripción</b></h6>
+    <small>${product.description}</small>
+  </div>
+  <div class="mb-4">
+    <h6 class="mb-0"><b>Categoría</b></h6>
+    <small>${product.category}</small>
+  </div>
+  <div class="mb-4">
+    <h6 class="mb-0"><b>Cantidad vendidos</b></h6>
+    <small>${product.soldCount}</small>
+  </div>
+  <div class="mb-4">
+    <h6 class="mb-2"><b>Imágenes ilustrativas</b></h6>
+    <div class="row mb-3">${showImages(product.images)}</div>
+  </div>
+</div>
+    `
+}
+
+function showImages(images) {
+    let img = "";
+
+    for (const image of images) {
+        img += `<div class="col-12 col-md-6 col-lg-3 mb-3"><img src="${image}" class="img-thumbnail img-fluid"></div>`
+    }
+    return img;
+}
   
