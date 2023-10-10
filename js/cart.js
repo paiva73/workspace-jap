df5aaf5d85bb5c377c26e6eac681135f46bb9198
Dolphin <128947588+IgnacioAlonsoSclavo@users.noreply.github.com>
2023-10-10 20:50:27 -0300

    Parte 3 completa (JS y HTML)

--- js/cart.js
+++ js/cart.js
@@ -2,23 +2,25 @@
 const CART_INFO_UR = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
 const cartContainer = document.getElementById("containerArticles");
 
+
 document.addEventListener("DOMContentLoaded", () => {
-   getAndShowCartArticles(CART_INFO_UR);
+  getAndShowCartArticles(CART_INFO_UR);
 })
 
-async function getAndShowCartArticles(url){
-    try { 
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`Hubo un problema con la solicitud: ${response.status}`);
-        }
-        const data = await response.json();
-        showCartArticles(data.articles);
-    } catch (error) {
-      console.log(error);
+async function getAndShowCartArticles(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Hubo un problema con la solicitud: ${response.status}`);
     }
+    const data = await response.json();
+    showCartArticles(data.articles);
+
+  } catch (error) {
+    console.log(error);
+  }
 }
-function showCartArticles(articles){
+function showCartArticles(articles) {
   for (const article of articles) {
     cartContainer.innerHTML += `
     <div class="row mb-4 d-flex justify-content-between align-items-center">
@@ -31,26 +33,58 @@ function showCartArticles(articles){
                         <h6 class="mb-0">${article.name}</h6>
                       </div>
                       <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                        <button class="btn btn-link px-2"
-                          onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                        <button class="btn btn-link px-2" id = "btnMen"
+                          onclick="funDown(${article.unitCost})">
                           <i class="fas fa-minus"></i>
                         </button>
-  
+
                         <input id="form1" min="0" name="quantity" value="1" type="number"
                           class="form-control form-control-sm" />
   
-                        <button class="btn btn-link px-2"
-                          onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                        <button class="btn btn-link px-2" id = "btnMas"Item
+                          onclick="funUp(${article.unitCost})">
                           <i class="fas fa-plus"></i>
                         </button>
                       </div>
                       <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                        <h6 class="mb-0">${article.currency} ${article.unitCost}</h6>
+                        <h6 class="mb-0">${article.currency} <span id="unity">${article.unitCost}</span></h6>
                       </div>
                       <div class="col-md-1 col-lg-1 col-xl-1 text-end">
                         <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
                       </div>
                     </div>
   `
+
   }
 }
+
+
+//Funcion para aumentar el "value" del form1
+function funUp(item) {
+  let form1 = document.getElementById("form1");
+  const unity = document.getElementById("unity");
+  unity.innerHTML = `
+  ${(parseFloat(form1.value)+1) * item}
+ `
+ form1.stepUp();
+}
+
+
+
+//Funcion  para disiminuir el value del form1 y validar que el resultado no sea negativo
+function funDown(item) {
+  let form1 = document.getElementById("form1");
+  const unity = document.getElementById("unity");
+  let result = parseFloat(form1.value)-1;
+  if(result<0){
+    result = 0;
+  }
+  unity.innerHTML = `
+  ${result * item}
+ `
+ form1.stepDown();
+}
+
+
+  
+
