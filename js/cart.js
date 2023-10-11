fa6fbb19bdf8d7236eeb45458c3203aac6cd423b
paiva73 <mau.paiva10@gmail.com>
2023-10-11 00:15:37 -0300

    Desafiate - Agregar productos al carrito. Completo
    - Agrego botón comprar.
    - Creo función para agregar productos al carrito en product-info.js.
    - Modifico funciones showCartArticles(), removeFromCart()  funUp y funDown en cart.js para adecuarlo al desafiate.

--- js/cart.js
+++ js/cart.js
@@ -1,21 +1,20 @@
-
 const CART_INFO_UR = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
 const cartContainer = document.getElementById("containerArticles");
-
+const productsInCart = JSON.parse(localStorage.getItem('productsInCart')) || [];
 
 document.addEventListener("DOMContentLoaded", () => {
   getAndShowCartArticles(CART_INFO_UR);
 })
 
-async function getAndShowCartArticles(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Hubo un problema con la solicitud: ${response.status}`);
-    }
-    const data = await response.json();
-    showCartArticles(data.articles);
-
+async function getAndShowCartArticles(url){
+  try { 
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Hubo un problema con la solicitud: ${response.status}`);
+      }
+      const data = await response.json();
+      showCartArticles(data.articles);
+      showCartArticles(productsInCart);
   } catch (error) {
     console.log(error);
   }
@@ -23,68 +22,73 @@ async function getAndShowCartArticles(url) {
 function showCartArticles(articles) {
   for (const article of articles) {
     cartContainer.innerHTML += `
-    <div class="row mb-4 d-flex justify-content-between align-items-center">
+                    <div id="${article.id}" class="row mb-4 d-flex justify-content-between align-items-center">
                       <div class="col-md-2 col-lg-2 col-xl-2">
                         <img
-                          src="${article.image}"
+                          src="${article.image || article.images[0]}"
                           class="img-fluid rounded-3" alt="Cotton T-shirt">
                       </div>
                       <div class="col-md-3 col-lg-3 col-xl-3">
                         <h6 class="mb-0">${article.name}</h6>
                       </div>
                       <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                        <button class="btn btn-link px-2" id = "btnMen"
-                          onclick="funDown(${article.unitCost})">
+                        <button class="btn btn-link px-2"
+                          onclick="funDown('${encodeURIComponent(JSON.stringify(article))}')">
                           <i class="fas fa-minus"></i>
                         </button>
 
-                        <input id="form1" min="0" name="quantity" value="1" type="number"
+                        <input id="form${article.id}" min="1" name="quantity" value="1" type="number"
                           class="form-control form-control-sm" />
   
-                        <button class="btn btn-link px-2" id = "btnMas"Item
-                          onclick="funUp(${article.unitCost})">
+                        <button class="btn btn-link px-2"Item
+                          onclick="funUp('${encodeURIComponent(JSON.stringify(article))}')">
                           <i class="fas fa-plus"></i>
                         </button>
                       </div>
                       <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                        <h6 class="mb-0">${article.currency} <span id="unity">${article.unitCost}</span></h6>
+                        <h6 class="mb-0">${article.currency} <span id="unity${article.id}">${article.unitCost || article.cost}</span></h6>
                       </div>
                       <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                        <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
+                      <a onclick="removeFromCart('${article.id}')" href="#!" class="text-muted"><i class="fas fa-times" ></i></a>
                       </div>
                     </div>
   `
-
   }
 }
-
-
 //Funcion para aumentar el "value" del form1
 function funUp(item) {
-  let form1 = document.getElementById("form1");
-  const unity = document.getElementById("unity");
+  let product = JSON.parse(decodeURIComponent(item));
+  let form1 = document.getElementById(`form${product.id}`);
+  const unity = document.getElementById(`unity${product.id}`);
   unity.innerHTML = `
-  ${(parseFloat(form1.value)+1) * item}
+  ${(parseFloat(form1.value)+1) * (product.cost || product.unitCost)}
  `
  form1.stepUp();
 }
-
-
-
 //Funcion  para disiminuir el value del form1 y validar que el resultado no sea negativo
 function funDown(item) {
-  let form1 = document.getElementById("form1");
-  const unity = document.getElementById("unity");
+  let product = JSON.parse(decodeURIComponent(item));
+  let form1 = document.getElementById(`form${product.id}`);
+  const unity = document.getElementById(`unity${product.id}`);
   let result = parseFloat(form1.value)-1;
-  if(result<0){
-    result = 0;
+  if(result<1){
+    result = 1;
   }
   unity.innerHTML = `
-  ${result * item}
+  ${result * (product.cost || product.unitCost)}
  `
  form1.stepDown();
 }
-
-
-  
-
+//Función que borra un producto del carrito.
+function removeFromCart(articleId) {
+  const articleElement = document.getElementById(articleId);
+  articleElement.remove();
+  //Borramos el producto del local storage.
+  for (const product of productsInCart) {
+    if(product.id == articleId){
+      let indice = productsInCart.indexOf(product);
+      productsInCart.splice(indice, 1);
+      localStorage.setItem('productsInCart', JSON.stringify(productsInCart));
+    }
+  }
+}
