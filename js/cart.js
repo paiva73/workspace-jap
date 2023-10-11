eb336d84eee78210d31239cdedaaa5267d0ceb13
paiva73 <mau.paiva10@gmail.com>
2023-10-10 23:19:17 -0300

    Revert "Desafiate - Agregar productos al carrito."
    This reverts commit e1e14cf7700307862ca402a6cf3ef93e33bd48e6.

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,5 @@
 const CART_INFO_UR = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
 const cartContainer = document.getElementById("containerArticles");
-const productsInCart = JSON.parse(localStorage.getItem('productsInCart')) || [];
 
 document.addEventListener("DOMContentLoaded", () => {
    getAndShowCartArticles(CART_INFO_UR);
@@ -14,7 +13,6 @@ async function getAndShowCartArticles(url){
         }
         const data = await response.json();
         showCartArticles(data.articles);
-        showCartArticles(productsInCart);
     } catch (error) {
       console.log(error);
     }
@@ -67,13 +65,15 @@ function cleanCart(){
 }
 */
 function showCartArticles(articles) {
+  let divCounter = 1; // Variable para mantener un contador único
   for (const article of articles) {
+    const articleId = `article-${divCounter}`;
     cartContainer.innerHTML += `
-      <div id="${article.id}" class="row mb-4 d-flex justify-content-between align-items-center">
+      <div id="${articleId}" class="row mb-4 d-flex justify-content-between align-items-center">
         <div class="col-md-2 col-lg-2 col-xl-2">
           <img
-            src="${article.image || article.images[0]}"
-            class="img-fluid rounded-3" alt="">
+            src="${article.image}"
+            class="img-fluid rounded-3" alt="Cotton T-shirt">
         </div>
         <div class="col-md-3 col-lg-3 col-xl-3">
           <h6 class="mb-0">${article.name}</h6>
@@ -93,25 +93,20 @@ function showCartArticles(articles) {
           </button>
         </div>
         <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-          <h6 class="mb-0">${article.currency} ${article.unitCost || article.cost}</h6>
+          <h6 class="mb-0">${article.currency} ${article.unitCost}</h6>
         </div>
         <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-          <a onclick="removeFromCart('${article.id}')" href="#!" class="text-muted"><i class="fas fa-times" ></i></a>
+          <a onclick="removeFromCart('${articleId}')" href="#!" class="text-muted"><i class="fas fa-times" ></i></a>
         </div>
       </div>
     `;
+    divCounter++; // Incrementa el contador para el siguiente div
   }
 }
-//Función que borra un producto del carrito.
+
 function removeFromCart(articleId) {
   const articleElement = document.getElementById(articleId);
-  articleElement.remove();
-  //Borramos el producto del local storage.
-  for (const product of productsInCart) {
-    if(product.id == articleId){
-      let indice = productsInCart.indexOf(product);
-      productsInCart.splice(indice, 1);
-      localStorage.setItem('productsInCart', JSON.stringify(productsInCart));
-    }
+  if (articleElement) {
+    articleElement.remove(); // Remove the article element from the DOM
   }
 }
