7e41a625a8d6086f7945a909bd4ccdfb7afc63c9
JPCdevila <Jpcdevila@gmail.com>
2023-10-13 17:26:58 -0300

    Fix to Cards (Responsive)

--- js/categories.js
+++ js/categories.js
@@ -52,10 +52,10 @@ function showCategoriesList(){
             htmlContentToAppend += `
             <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
-                    <div class="col-3">
+                    <div class="col-md-3">
                         <img src="${category.imgSrc}" alt="${category.description}" class="img-thumbnail">
                     </div>
-                    <div class="col">
+                    <div class="col-md-6">
                         <div class="d-flex w-100 justify-content-between">
                             <h4 class="mb-1">${category.name}</h4>
                             <small class="text-muted">${category.productCount} artículos</small>
--- js/products.js
+++ js/products.js
@@ -20,10 +20,10 @@ document.addEventListener("DOMContentLoaded", () => {
       containerProducts.innerHTML += `
       <div onclick="setProductID(${product.id})" class="list-group-item list-group-item-action cursor-active">
           <div class="row">
-              <div class="col-3">
+              <div class="col-md-3">
                   <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
               </div>
-              <div class="col">
+              <div class="col-md-6">
                 <div class="d-flex w-100 justify-content-between">
                   <h4 class="mb-1">${product.name} - ${product.currency} ${product.cost}</h4>
                   <small class="text-muted">${product.soldCount} artículos</small>
