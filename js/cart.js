c14a13cb6628333b995d5ca9e63cfa3bda69dd46
BardesioDiego <diegobardesio@gmail.com>
2023-10-24 21:40:47 -0300

    Parte 3-Segundo cambio

--- js/cart.js
+++ js/cart.js
@@ -133,21 +133,22 @@ document.addEventListener("DOMContentLoaded", function() {
 */
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
+(function (){
   'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.needs-validation')
 
   // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event){
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-      form.classList.add('was-validated')
-    }, false)
+        form.classList.add('was-validated')
+      }, false)
   })
 })()
